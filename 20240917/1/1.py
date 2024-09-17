c0bddadfef32766ad867a55a90b6fc309631b0a2
priestprog <abagrov04@gmail.com>
2024-09-17 12:00:08 +0300

    prac 3

--- 20240917/1/1.py
+++ 20240917/1/1.py
@@ -1,16 +1,14 @@
 a = eval(input())
-if  a % 25 == 0:
-    
-    
-match a:
-    case 1:
-        print("1111")
-    case 2:
-        print("2222")
-    case 3:
-        print("33333")
-    case c:
-        if c%2==0:
-            print("even")
-        else:
-            print("N - moreeee")
+if a % 25 == 0:
+    if a % 2 == 0:
+        print("A + B -", end=" ")
+    else:
+        print("A - B +", end=" ")
+else:
+    print("A - B -", end=" ")
+
+if a % 8 == 0:
+    print("C +")
+else:
+    print("C -")
+
