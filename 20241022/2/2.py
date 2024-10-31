05bf1d2ddcd9e6faba529cb50412065296aa556a
priestprog <abagrov04@gmail.com>
2024-10-31 14:15:14 +0300

    zatupil, spasibo George Kouryachy

--- 20241022/2/2.py
+++ 20241022/2/2.py
@@ -1,7 +1,7 @@
 import itertools
 
 def slide(seq, n):
-    iters = list(itertools.tee(seq, len(seq)-n+1))
+    iters = list(itertools.tee(seq, len(seq)))
     for i, it in enumerate(iters):
         window = itertools.islice(it, i, n+i)
         yield from window
