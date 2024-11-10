fcd2e4c36e85df39dd19226e53d715d75aa57e96
priestprog <abagrov04@gmail.com>
2024-11-10 14:13:10 +0300

    homework to 5.11 part 1

--- 20241105/1/1.py
+++ 20241105/1/1.py
@@ -1,17 +1,29 @@
-def fib(m,n):
-    num = 0
-    flag = n
-    temp1, temp2 = 1, 1
-    while True:
-        if flag == 0:
-            break
-        if num >= m:
-            yield temp1
-            flag -= 1
-        tmp = temp1
-        temp1 = temp2
-        temp2 += tmp
-        num += 1
+class Omnibus:
+    attrs_count = {}
+
+    def __init__(self):
+        self.local_attrs = set()
+
+    def __setattr__(self, name, value):
+        if name == 'local_attrs':
+            super().__setattr__(name, value)
+            return
+        if name not in self.__class__.attrs_count:
+            self.__class__.attrs_count[name] = 1
+        else:
+            self.__class__.attrs_count[name] += 1
+        self.local_attrs.add(name)
+
+    def __getattr__(self, name):
+        if name in self.__class__.attrs_count:
+            return self.__class__.attrs_count[name]
+
+    def __delattr__(self, name):
+        if name in self.local_attrs:
+            self.local_attrs.remove(name)
+            self.__class__.attrs_count[name] -= 1
+            if self.__class__.attrs_count[name] == 0:
+                del self.__class__.attrs_count[name]
 
 import sys
 exec(sys.stdin.read())
--- 20241105/2/2.py
+++ 20241105/2/2.py
@@ -1,10 +1,39 @@
-import itertools
+import numpy as np
 
-def slide(seq, n):
-    iters = list(itertools.tee(seq, len(seq)))
-    for i, it in enumerate(iters):
-        window = itertools.islice(it, i, n+i)
-        yield from window
+class Triangle:
+    def __init__(self, p1, p2, p3):
+        self.points = np.array([p1, p2, p3], dtype=float)
+        self.points = np.hstack((self.points, np.zeros((3, 1))))
+
+    def __abs__(self):
+        a, b, c = self.points
+        return abs(np.cross(b - a, c - a)[-1]) / 2
+
+    def __bool__(self):
+        return bool(not np.isclose(abs(self), 0))
+
+    def __lt__(self, other):
+        return bool(abs(self) < abs(other))
+
+    def contains_point(self, point):
+        point = np.array([*point, 0])
+        a, b, c = self.points
+        v1 = np.cross(b - a, point - a)[-1]
+        v2 = np.cross(c - b, point - b)[-1]
+        v3 = np.cross(a - c, point - c)[-1]
+        return (v1 >= 0 and v2 >= 0 and v3 >= 0) or (v1 <= 0 and v2 <= 0 and v3 <= 0)
+
+    def __contains__(self, other):
+        if not other:
+            return True
+        if not self:
+            return False
+        return all(self.contains_point(pt[:2]) for pt in other.points)
+
+    def __and__(self, other):
+        if not self or not other:
+            return False
+        return any(self.contains_point(pt[:2]) for pt in other.points) or any(other.contains_point(pt[:2]) for pt in self.points)
 
 import sys
 exec(sys.stdin.read())
